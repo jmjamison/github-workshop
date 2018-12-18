5882ed52b0414e66d497d99dcdfed9266b44cbca
Jamie Jamison <jamie@jmjamison.com>
2018-12-17 19:18:42 -0800

    Editing and adding notes

--- r-ladies-resentation-dec-17-2018.R
+++ r-ladies-resentation-dec-17-2018.R
@@ -1,6 +1,9 @@
 # r-ladies presentation 12/17/2018
 # http://happygitwithr.com/
 
-Don't push the metadata, may have passwords. Rstudio setup file includes .gitignore and takes care of this.
+# Don't push the metadata, may have passwords. Rstudio setup file includes .gitignore and takes care of this.
+
+# https://github.com/jmjamison/github-workshop.git
 
 
+# should show why you did as well as what
