ff3150ee4c74c775d57ea55dfcd5fc2f77a6f929
Jamie Jamison <jamie@jmjamison.com>
2018-12-17 19:44:19 -0800

    notes about adding

--- r-ladies-resentation-dec-17-2018.R
+++ r-ladies-resentation-dec-17-2018.R
@@ -1,6 +1,6 @@
 # r-ladies presentation 12/17/2018
 # http://happygitwithr.com/
-
+# git integration in RStudio
 # Don't push the metadata, may have passwords. Rstudio setup file includes .gitignore and takes care of this.
 
 # https://github.com/jmjamison/github-workshop.git
